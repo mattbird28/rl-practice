a42a45f8ac86c95853bdd507f37a9208b4ce9753
Matt Bird <mattibird2@gmail.com>
2026-03-19 17:16:56 +0000

    Adding pygame for rendering

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import gymnasium as gym
 
-env = gym.make("CartPole-v1")
+env = gym.make("CartPole-v1", render_mode="human")
 obs, info = env.reset()
 
-for step in range(10):
+for step in range(200):
     action = env.action_space.sample()
     obs, reward, terminated, truncated, info = env.step(action)
-    print(step, reward)
+    print(
+    f"step={step} | "
+    f"action={action} | "
+    f"obs={obs} | "
+    f"reward={reward} | "
+    f"terminated={terminated} | "
+    f"truncated={truncated}")
 
     if terminated or truncated:
         obs, info = env.reset()
